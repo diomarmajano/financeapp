90a91ff548809d68b5ab1b898ba6975ab8b9dd33
Diomar Majano <http.diomar@gmail.com>
2025-09-15 13:03:15 -0300

    creacion de vistas y logicas de los botones

--- app/src/main/java/com/example/finance/MainActivity.kt
+++ app/src/main/java/com/example/finance/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.finance
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,41 +8,56 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
 import com.example.finance.ui.theme.FinanceTheme
+import com.example.finance.resources.views.HomeScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.finance.resources.views.RecoverScreen
+import com.example.finance.resources.views.RegisterScreen
+import com.example.finance.resources.views.WelcomeScreen
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FinanceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val navController = rememberNavController()
+                val usuariosPredeterminados = remember {
+                    mutableStateListOf(
+                        "diomar" to "diomar123",
+                        "luz" to "luz123",
+                        "gregory" to "gregory123",
+                        "maria" to "maria123",
+                        "edgar" to "edgar123",
                     )
+                }
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login",
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable("login") { HomeScreen(navController, usuariosPredeterminados) }
+                        composable("register") { RegisterScreen(navController, usuariosPredeterminados)}
+                        composable("recover") { RecoverScreen(navController, usuariosPredeterminados)}
+                        composable("welcome") { WelcomeScreen(navController) }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FinanceTheme {
-        Greeting("Android")
-    }
-}
+
+
+
---
+++ app/src/main/java/com/example/finance/resources/views/WelcomeScreen.kt
@@ -0,0 +1,69 @@
+package com.example.finance.resources.views
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.finance.ui.theme.PersonalTheme
+
+@Composable
+fun WelcomeScreen(navController: NavHostController) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(PersonalTheme.backgroundPrimary)
+            .padding(24.dp)
+            .semantics { contentDescription = "Pantalla de bienvenida" },
+        verticalArrangement = Arrangement.Center
+    )
+    {
+        //Mensaje principal de la pantalla de inicio
+        Text(
+            text = "¡Bienvenido a tu aplicacion de finanzas, próximamente tendremos novedades para tí!",
+            color = PersonalTheme.primaryColor,
+            fontFamily = PersonalTheme.TypeText,
+            fontSize = 30.sp,
+            modifier = Modifier.padding(bottom = 32.dp)
+        )
+
+        Button(
+            { navController.navigate("login")},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 30.dp),
+            shape = RoundedCornerShape(16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = PersonalTheme.primaryColor,
+                contentColor = PersonalTheme.TextPrimary
+            )
+        ) {
+            Text("Cerrar sesión")
+        }
+    }
+}
+
